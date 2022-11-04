ee9b9d1dda27a3a03140ac56c8ab30d3519d06cb
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-11-04 03:11:38 -0300

    feat: app.post sign-up

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.listen(5000, () => console.log("Server running in port 5000"))
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+	const { username, avatar } = req.body;
+
+	users.push({ username, avatar });
+
+	res.send("OK");
+});
+
+app.listen(5000, () => console.log("App running in port 5000"));
